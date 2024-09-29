c7754cdec19fbcdaf96f698bd5d5b43e4203f041
MestoloPomello <stefanonava99@gmail.com>
2024-09-30 00:06:52 +0200

    sticky scroll context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,23 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function ()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true }
-		})
-	end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function ()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true }
+			})
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function ()
+			local contextConfig = require("treesitter-context")
+			contextConfig.setup({
+				enable = true
+			})
+		end
+	}
 }
-
