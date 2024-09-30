9b3af7139b63a50af00558b45423aed39f9bf1ef
MestoloPomello <stefanonava99@gmail.com>
2024-09-30 15:20:57 +0200

    none-ls config update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,19 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.eslint_d,
+				-- null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.formatting.stylua,
 			},
 		})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
 	end,
 }
