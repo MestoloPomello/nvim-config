e0af617d4f8e0b1e896a5ee810142010933e65dc
MestoloPomello <stefanonava99@gmail.com>
2024-10-07 20:35:27 +0200

    removed smartcase

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,8 +4,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-vim.cmd("set ignorecase")
-vim.cmd("set smartcase")
+-- vim.cmd("set ignorecase")
+-- vim.cmd("set smartcase")
 vim.g.mapleader = " "
 
 -- Commands
