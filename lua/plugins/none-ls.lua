0209f9f7827305a3a7dbae919aea921e8ea732c9
MestoloPomello <stefanonava99@gmail.com>
2024-11-08 22:24:53 +0100

    remove eslint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.prettier,
 				-- null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.formatting.stylua,
