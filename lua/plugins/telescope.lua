5af293579112b18bdf5556596d42ec637d04903a
Stefano Nava <stefano.nava@sentric.it>
2024-11-12 09:30:27 +0100

    add shortened file picker for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,23 @@ return {
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function ()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "TelescopeResults",
+				callback = function(ctx)
+					vim.api.nvim_buf_call(ctx.buf, function()
+						vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+						vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+					end)
+				end,
+			})
+
+			local function filenameFirst(_, path)
+				local tail = vim.fs.basename(path)
+				local parent = vim.fs.dirname(path)
+				if parent == "." then return tail end
+				return string.format("%s\t\t%s", tail, parent)
+			end
+
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
@@ -20,6 +37,11 @@ return {
 				},
 				file_ignore_patterns = {
 					"^node%_modules/.*"
+				},
+				pickers = {
+					find_files = {
+						path_display = filenameFirst,
+					}
 				}
 			})
 			require("telescope").load_extension("ui-select")
