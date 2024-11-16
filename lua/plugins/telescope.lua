b0f481e90979bd476f08d56be66d57d6d3afb6b0
MestoloPomello <stefanonava99@gmail.com>
2024-11-17 00:38:07 +0100

    feat: native filename_first for telescope and live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		'nvim-telescope/telescope.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require("telescope.builtin")
@@ -11,29 +11,10 @@ return {
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function ()
-			-- Part of the path_display workaround
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "TelescopeResults",
-				callback = function(ctx)
-					vim.api.nvim_buf_call(ctx.buf, function()
-						vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-						vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-					end)
-				end,
-			})
-
-			-- Part of the path_display workaround
-			local function filenameFirst(_, path)
-				local tail = vim.fs.basename(path)
-				local parent = vim.fs.dirname(path)
-				if parent == "." then return tail end
-				return string.format("%s\t\t%s", tail, parent)
-			end
-
 			require("telescope").setup({
 				defaults = {
 					layout_strategy = "vertical",
-					-- path_display = { "filename_first" } -- Uncomment when it starts working
+					path_display = { "filename_first" }
 				},
 				extensions = {
 					["ui-select"] = {
@@ -44,11 +25,6 @@ return {
 				file_ignore_patterns = {
 					"^node%_modules/.*"
 				},
-				pickers = { -- Part of the path_display workaround
-					find_files = {
-						path_display = filenameFirst,
-					}
-				}
 			})
 			require("telescope").load_extension("ui-select")
 			-- require("telescope").load_extension("projects")
