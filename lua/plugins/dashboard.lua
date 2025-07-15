d7994efdaf35c129cefce6869b78ab6d548bca05
MestoloPomello <stefanonava99@gmail.com>
2025-07-16 01:51:17 +0200

    feat: nvim-dashboard (commented)

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,24 @@
+-- return {
+--   'nvimdev/dashboard-nvim',
+--   event = 'VimEnter',
+--   config = function()
+--     require('dashboard').setup {
+-- 		theme = "hyper",
+-- 		config = {
+-- 			-- https://patorjk.com/software/taag/#p=testall&f=Big&t=PORCA%20TROIA
+-- 			header = {
+-- 				"▗▄▄▖  ▗▄▖ ▗▄▄▖  ▗▄▄▖ ▗▄▖     ▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▄▄▄▖ ▗▄▖ ",
+-- 				"▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌      █  ▐▌ ▐▌▐▌ ▐▌  █  ▐▌ ▐▌",
+-- 				"▐▛▀▘ ▐▌ ▐▌▐▛▀▚▖▐▌   ▐▛▀▜▌      █  ▐▛▀▚▖▐▌ ▐▌  █  ▐▛▀▜▌",
+-- 				"▐▌   ▝▚▄▞▘▐▌ ▐▌▝▚▄▄▖▐▌ ▐▌      █  ▐▌ ▐▌▝▚▄▞▘▗▄█▄▖▐▌ ▐▌",
+-- 				"",
+-- 				"",
+-- 				"\"Se caghi pisci, ma se pisci non è detto che caghi.\"",
+-- 				"",
+-- 				""
+-- 			}
+-- 		}
+--     }
+--   end,
+--   dependencies = { {'nvim-tree/nvim-web-devicons'}}
+-- }
