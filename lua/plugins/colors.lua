7baabae254078739b762f3885817efef7a8b6073
MestoloPomello <stefanonava99@gmail.com>
2025-07-22 01:36:27 +0200

    feat: color schemes and dashboard logo

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,12 +7,34 @@
 -- 		vim.cmd.colorscheme "catppuccin"
 -- 	end
 -- }
+
 return {
 	'navarasu/onedark.nvim',
 	config = function()
 		require('onedark').setup {
-			style = 'warm'
+			style = 'warmer'
 		}
 		require('onedark').load()
 	end
 }
+
+-- return {
+-- 	"bluz71/vim-moonfly-colors",
+-- 	name = "moonfly",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd.colorscheme "moonfly"
+-- 	end
+-- }
+
+-- return {
+--     "slugbyte/lackluster.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     init = function()
+--         -- vim.cmd.colorscheme("lackluster")
+--         -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+--         vim.cmd.colorscheme("lackluster-mint")
+--     end,
+-- }
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,24 +1,33 @@
--- return {
---   'nvimdev/dashboard-nvim',
---   event = 'VimEnter',
---   config = function()
---     require('dashboard').setup {
--- 		theme = "hyper",
--- 		config = {
--- 			-- https://patorjk.com/software/taag/#p=testall&f=Big&t=PORCA%20TROIA
--- 			header = {
--- 				"▗▄▄▖  ▗▄▖ ▗▄▄▖  ▗▄▄▖ ▗▄▖     ▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▄▄▄▖ ▗▄▖ ",
--- 				"▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌      █  ▐▌ ▐▌▐▌ ▐▌  █  ▐▌ ▐▌",
--- 				"▐▛▀▘ ▐▌ ▐▌▐▛▀▚▖▐▌   ▐▛▀▜▌      █  ▐▛▀▚▖▐▌ ▐▌  █  ▐▛▀▜▌",
--- 				"▐▌   ▝▚▄▞▘▐▌ ▐▌▝▚▄▄▖▐▌ ▐▌      █  ▐▌ ▐▌▝▚▄▞▘▗▄█▄▖▐▌ ▐▌",
--- 				"",
--- 				"",
--- 				"\"Se caghi pisci, ma se pisci non è detto che caghi.\"",
--- 				"",
--- 				""
--- 			}
--- 		}
---     }
---   end,
---   dependencies = { {'nvim-tree/nvim-web-devicons'}}
--- }
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+		theme = "hyper",
+		config = {
+			-- https://patorjk.com/software/taag/#p=testall&f=Big&t=PORCA%20TROIA
+			-- header = {
+			-- 	"▗▄▄▖  ▗▄▖ ▗▄▄▖  ▗▄▄▖ ▗▄▖     ▗▄▄▄▖▗▄▄▖  ▗▄▖ ▗▄▄▄▖ ▗▄▖ ",
+			-- 	"▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌      █  ▐▌ ▐▌▐▌ ▐▌  █  ▐▌ ▐▌",
+			-- 	"▐▛▀▘ ▐▌ ▐▌▐▛▀▚▖▐▌   ▐▛▀▜▌      █  ▐▛▀▚▖▐▌ ▐▌  █  ▐▛▀▜▌",
+			-- 	"▐▌   ▝▚▄▞▘▐▌ ▐▌▝▚▄▄▖▐▌ ▐▌      █  ▐▌ ▐▌▝▚▄▞▘▗▄█▄▖▐▌ ▐▌",
+			-- 	"",
+			-- 	"",
+			-- 	"\"Se caghi pisci, ma se pisci non è detto che caghi.\"",
+			-- 	"",
+			-- 	""
+			-- }
+			header = {
+				"███    ██ ███████  ██████  ██    ██ ██ ███    ███ ",
+				"████   ██ ██      ██    ██ ██    ██ ██ ████  ████ ",
+				"██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██ ",
+				"██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██ ",
+				"██   ████ ███████  ██████    ████   ██ ██      ██ ",
+				"",
+				""
+			}
+		}
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
